0930adb2ee1c3b7d18dcdec8ce194058e0665168
Faizan Virani <fvirani6@gatech.edu>
2014-10-19 05:06:09 -0400

    Got the HipsterIndex

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var totalHipsterScore;
-var hipsterIndex;
-var totalImportantQuestions;
+var totalHipsterScore = 0;
+var hipsterIndex = 0;
+var totalImportantQuestions = 0;
 
 //Genre scale for each level of hipster
 var genres = ["pop", "indie pop", "indie rock", "folk", "hipster"];
@@ -13,28 +13,35 @@ $(function(){
 		for(i = 0; i < questions.length; i++) {
 			if(questions[i].type != "control") {
 				totalImportantQuestions++;
-				;
 			}
 			//Add question to HTML form
 			$("#survey").append("<div class=\"questionBlock\">");
 			$("#survey").append("<span class=\"question\">" + questions[i].question + "</span>");
 			if(questions[i].type == "normal") {
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\ value=\"1\">1<br />");
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\ value=\"2\">2<br />");
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\ value=\"3\">3<br />");
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\ value=\"4\">4<br />");
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\ value=\"5\">5<br />");
+				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"1\">1<br />");
+				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"2\">2<br />");
+				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"3\">3<br />");
+				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"4\">4<br />");
+				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"5\">5<br />");
 			} else if(questions[i].type == "anti") {
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\ value=\"5\">1<br />");
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\ value=\"4\">2<br />");
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\ value=\"3\">3<br />");
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\ value=\"2\">4<br />");
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\ value=\"1\">5<br />");
+				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"5\">1<br />");
+				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"4\">2<br />");
+				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"3\">3<br />");
+				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"2\">4<br />");
+				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"1\">5<br />");
 			} else if(questions[i].type == 'yesNo'){
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\ value=\"5\">Yes<br />");
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\ value=\"1\">No<br />");
+				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"5\">Yes<br />");
+				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"1\">No<br />");
+
+			} else if(questions[i].type == 'control'){
+				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"0\">1<br />");
+				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"0\">2<br />");
+				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"0\">3<br />");
+				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"0\">4<br />");
+				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"0\">5<br />");
 			}
 		}
+		$("#survey").append("<input type=\"button\" onclick=\"calculateAndDoHipster()\" value=\"Submit\">");
 	}
 
 })
@@ -45,10 +52,11 @@ function calculateAndDoHipster() {
 }
 
 function calculateHipsterIndex() {
-	for(i = 0; i < question.length; i++) {
-		totalHipsterScore += $("input:radio[name=question" + i + "]:checked").val();
+	for(i = 0; i < questions.length; i++) {
+		console.log(parseInt($("input:radio[name=question" + i + "]:checked").val()));
+		totalHipsterScore += parseInt($("input:radio[name=question" + i + "]:checked").val());
 	}
-	hipsterIndex = totalHipsterScore / totalImportantQuestions;
+	hipsterIndex = Math.round(totalHipsterScore / totalImportantQuestions);
 }
 
 function getHipsterMusic() {
