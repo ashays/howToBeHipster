871f8f5f6e85f128597975627ee50fc7a2750015
ashays <ashay.sheth@gmail.com>
2014-10-19 03:50:56 -0400

    Added API call

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-$(function(){
-
 var totalHipsterScore;
 var hipsterIndex;
 var totalImportantQuestions;
@@ -8,17 +6,30 @@ var totalImportantQuestions;
 var genres = ["pop", "indie pop", "indie rock", "folk", "hipster"];
 var genreCodes = [0, 0, 0, 0, 0]; //Code for genre in Music Dealers API 
 
-setUpSurvey();
+$(function(){
+	setUpSurvey();
 
-function setUpSurvey() {
-	for(i = 0; i < questions.length; i++) {
-		if(questions[i].type != "control") {
-			totalImportantQuestions++;
+	function setUpSurvey() {
+		for(i = 0; i < questions.length; i++) {
+			if(questions[i].type != "control") {
+				totalImportantQuestions++;
+			}
+			//Add question to HTML form
+			$("#survey").append("<div class=\"questionBlock\">");
+			$("#survey").append("<span class=\"question\">" + questions[i].question + "</span>");
+			if(questions[i].type == "normal") {
+				//append all the answer choices with the values matching the labels
+			} else if(questions[i].type == "anti") {
+				//append all the answer choices except with the values inversed of the labels
+			} //add else if the question type is the yes/no thing
 		}
-		//Add question to HTML form
-		$("#survey").append("<div class=\"questionBlock\">");
-		$("#survey").append("<span class=\"question\">" + questions[i].question + "</span>");
 	}
+
+})
+
+function calculateAndDoHipster() {
+	calculateHipsterIndex();
+
 }
 
 function calculateHipsterIndex() {
@@ -28,4 +39,19 @@ function calculateHipsterIndex() {
 	hipsterIndex = totalHipsterScore / totalImportantQuestions;
 }
 
-})
+function getHipsterMusic() {
+	var genres = genreCodes[hipsterIndex - 1];
+
+	//Authentication
+	$.ajax({
+	  type: "POST",
+	  url: "http://api.musicdealers.com/authentication/login",
+	  data: { username: "ashay@gatech.edu", password: "brownteam" },
+	});
+
+	$.ajax({
+	  type: "POST",
+	  url: "http://api.musicdealers.com/songs/{1,1}",
+	  data: genres,
+	});
+}
