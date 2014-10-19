4f96eb187ea84d6cdd0fb8f22dd9946bc596dfe5
ashays <ashay.sheth@gmail.com>
2014-10-19 10:54:21 -0400

    connecting to echo nest

--- main.js
+++ main.js
@@ -10,36 +10,38 @@ $(function(){
 	setUpSurvey();
 
 	function setUpSurvey() {
+		var HTMLinputStart = "<li><input type=\"radio\" name=\"";
 		for(i = 0; i < questions.length; i++) {
 			if(questions[i].type != "control") {
 				totalImportantQuestions++;
 			}
 			//Add question to HTML form
+			var HTMLnameFor = "question" + i;
 			$("#survey").append("<div class=\"questionBlock\">");
-			$("#survey").append("<span class=\"question\">" + questions[i].question + "</span>");
+			$("#survey").append("<span class=\"question\">" + questions[i].question + "</span><ul>");
 			if(questions[i].type == "normal") {
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"1\">1<br />");
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"2\">2<br />");
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"3\">3<br />");
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"4\">4<br />");
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"5\">5<br />");
+				$("#survey").append(HTMLinputStart + HTMLnameFor + "\" id=\"" + HTMLnameFor + "\" value=\"1\"><label for=\"" + HTMLnameFor + "\">1</label></li>");
+				$("#survey").append(HTMLinputStart + HTMLnameFor + "\" value=\"2\"><label for=\"" + HTMLnameFor + "\">2</label></li>");
+				$("#survey").append(HTMLinputStart + HTMLnameFor + "\" value=\"3\"><label for=\"" + HTMLnameFor + "\">3</label></li>");
+				$("#survey").append(HTMLinputStart + HTMLnameFor + "\" value=\"4\"><label for=\"" + HTMLnameFor + "\">4</label></li>");
+				$("#survey").append(HTMLinputStart + HTMLnameFor + "\" value=\"5\"><label for=\"" + HTMLnameFor + "\">5</label></li>");
 			} else if(questions[i].type == "anti") {
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"5\">1<br />");
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"4\">2<br />");
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"3\">3<br />");
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"2\">4<br />");
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"1\">5<br />");
+				$("#survey").append(HTMLinputStart + HTMLnameFor + "\" value=\"5\"><label for=\"" + HTMLnameFor + "\">1</label></li>");
+				$("#survey").append(HTMLinputStart + HTMLnameFor + "\" value=\"4\"><label for=\"" + HTMLnameFor + "\">2</label></li>");
+				$("#survey").append(HTMLinputStart + HTMLnameFor + "\" value=\"3\"><label for=\"" + HTMLnameFor + "\">3</label></li>");
+				$("#survey").append(HTMLinputStart + HTMLnameFor + "\" value=\"2\"><label for=\"" + HTMLnameFor + "\">4</label></li>");
+				$("#survey").append(HTMLinputStart + HTMLnameFor + "\" value=\"1\"><label for=\"" + HTMLnameFor + "\">5</label></li>");
 			} else if(questions[i].type == 'yesNo'){
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"5\">Yes<br />");
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"1\">No<br />");
-
+				$("#survey").append(HTMLinputStart + HTMLnameFor + "\" value=\"5\"><label for=\"" + HTMLnameFor + "\">Yes</label></li>");
+				$("#survey").append(HTMLinputStart + HTMLnameFor + "\" value=\"1\"><label for=\"" + HTMLnameFor + "\">No</label></li>");
 			} else if(questions[i].type == 'control'){
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"0\">1<br />");
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"0\">2<br />");
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"0\">3<br />");
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"0\">4<br />");
-				$("#survey").append("<input type=\"radio\" name=\"question" + i + "\" value=\"0\">5<br />");
+				$("#survey").append(HTMLinputStart + HTMLnameFor + "\" value=\"0\"><label for=\"" + HTMLnameFor + "\">1</label></li>");
+				$("#survey").append(HTMLinputStart + HTMLnameFor + "\" value=\"0\"><label for=\"" + HTMLnameFor + "\">2</label></li>");
+				$("#survey").append(HTMLinputStart + HTMLnameFor + "\" value=\"0\"><label for=\"" + HTMLnameFor + "\">3</label></li>");
+				$("#survey").append(HTMLinputStart + HTMLnameFor + "\" value=\"0\"><label for=\"" + HTMLnameFor + "\">4</label></li>");
+				$("#survey").append(HTMLinputStart + HTMLnameFor + "\" value=\"0\"><label for=\"" + HTMLnameFor + "\">5</label></li>");
 			}
+			$("#survey").append("</ul></div>");
 		}
 		$("#survey").append("<input type=\"button\" onclick=\"calculateAndDoHipster()\" value=\"Submit\">");
 	}
@@ -48,7 +50,7 @@ $(function(){
 
 function calculateAndDoHipster() {
 	calculateHipsterIndex();
-
+	getHipsterMusic();
 }
 
 function calculateHipsterIndex() {
@@ -62,16 +64,43 @@ function calculateHipsterIndex() {
 function getHipsterMusic() {
 	var genres = genreCodes[hipsterIndex - 1];
 
+// var xhr = new XMLHttpRequest();
+// xhr.open("POST", "https://private-anon-d150c1785-mdlrs.apiary-mock.com/authentication/login");
+// xhr.onreadystatechange = function () {
+//   if (this.readyState == 4) {
+//     alert('Status: '+this.status+'\nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'\nBody: '+this.responseText);
+//   }
+// };
+// xhr.send("Content-Type: multipart/form-data; boundary=----WebKitFormBoundaryp7MA4YWxkTrZu0gW\n----WebKitFormBoundaryE19zNvXGzXaLvS5C\nContent-Disposition: form-data; name=\"username\"\nfvirani6@gatech.edu\n----WebKitFormBoundaryE19zNvXGzXaLvS5C\nContent-Disposition: form-data; name=\"password\"\nfv0404\n----WebKitFormBoundaryE19zNvXGzXaLvS5C");	
+
 	//Authentication
 	$.ajax({
 	  type: "POST",
 	  url: "http://api.musicdealers.com/authentication/login",
-	  data: { username: "ashay@gatech.edu", password: "brownteam" },
+	  data: { username: "faizan.virani.44@gmail.com", password: "fv0404" },
+	  success: function(resp) {
+	  	console.log("music dealers success")
+	  	$.ajax({
+	  		type: "POST",
+	  		url: "http://api.musicdealers.com/songs/{1,1}",
+	  		data: genres,
+		});
+	  }
 	});
 
+
 	$.ajax({
-	  type: "POST",
-	  url: "http://api.musicdealers.com/songs/{1,1}",
-	  data: genres,
-	});
+  url: "http://developer.echonest.com/api/v4/song/search",
+  data: {
+  	'api_key': 'KL8LOBUKZKX4SDIXK',
+  	'results': '1',
+  	'style': genres[hipsterIndex],
+  	'bucket': 'tracks'
+  },
+  dataType: 'json',
+  success: function (resp) {
+  	console.log("successful"),
+  	console.log(resp.response.songs[0].artist_name)
+  }
+});
 }
